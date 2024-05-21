cdc825834567686f98f5f0c35d08fca61c32e8ed
chuntaopeng <pengtaiwan6517@gmail.com>
2024-05-21 21:26:38 +0800

    feat:adding my post button

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import MyPostButton from "./MyPostButton";
 interface ProfileProps {
     title: string;
     phone: string;
@@ -8,7 +8,7 @@ interface ProfileProps {
 
 const Profile: React.FC<ProfileProps> = ({ title, phone, mail }) => {
   return (
-    <div className="flex justify-center items-center h-screen w-8/12">
+    <div className="flex flex-col items-center  w-8/12 h-full mt-44">
       <div className="w-2/3 h-2/5 bg-white shadow-md p-8 rounded-xl border-4 border-black flex items-center flex-col">
         <div className="mb-4 h-1/3 flex items-center">
           <img src="/app_images/people.png" alt="Profile" className="w-16 h-16 mr-4" />
@@ -23,6 +23,7 @@ const Profile: React.FC<ProfileProps> = ({ title, phone, mail }) => {
           <h1 className="text-2xl font-bold">{mail}</h1>
         </div>
       </div>
+      <MyPostButton />
     </div>
   );
 };
