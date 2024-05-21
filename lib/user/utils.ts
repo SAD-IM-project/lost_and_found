65f9afb27ea5f8298b56dfc08e4348a2dbe42a07
ansonwuuuuu <ansonwu0604@gmail.com>
2024-05-21 20:15:53 +0800

    feat: add utils for user and object

---
+++ lib/user/utils.ts
@@ -0,0 +1,33 @@
+import type { SupabaseClient } from "@supabase/supabase-js";
+
+export async function getUser(supabase: SupabaseClient, user_id: string) {
+    const { data, error } = await supabase
+        .from("users")
+        .select("*")
+        .eq("user_id", user_id);
+    if (error) {
+        throw error;
+    }
+    return data ? data[0] : null;
+}
+
+export async function createUser(supabase: SupabaseClient, user: Partial<User>) {
+    if (!user.user_id) {
+        throw new Error("User ID is required");
+    }
+    if (!user.user_name) {
+        throw new Error("User name is required");
+    }
+    if (user.user_name == '') {
+        throw new Error("User name cannot be empty");
+    }
+
+    const { data, error } = await supabase
+        .from("users")
+        .upsert([user])
+        .select("*");
+    if (error) {
+        throw error;
+    }
+    return data;
+}
