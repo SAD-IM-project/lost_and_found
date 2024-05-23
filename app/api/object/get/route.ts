006c6ddd847d3b925fe4f23e989c48bba3bfc826
ansonwuuuuu <ansonwu0604@gmail.com>
2024-05-23 13:07:15 +0800

    feat: fix object get

--- app/api/object/get/route.ts
+++ app/api/object/get/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: NextRequest) {
     
     const object_id = searchParams.get("object_id");
     const search = searchParams.get("search");
-    console.log(object_id, search)
+    // console.log(object_id, search)
     
     if (!object_id) {
         return NextResponse.json({ message: "object_id is required" }, { status: 400 });
@@ -24,11 +24,8 @@ export async function GET(request: NextRequest) {
             const data = await getObjects(supabase);
             return NextResponse.json(data, { status: 200 });
         }
-        const { data, error } = await getObject(supabase, object_id);
-        if (error) {
-        return NextResponse.json(error, { status: 500 });
-        }
-    
+        const data = await getObject(supabase, object_id);
+        
         return NextResponse.json(data, { status: 200 });
     } catch (error) {
         return NextResponse.json(error, { status: 500 });
