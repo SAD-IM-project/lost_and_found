990c615f234bf86d7ed3ab6baab50193ae515035
ansonwuuuuu <ansonwu0604@gmail.com>
2024-05-25 18:41:54 +0800

    style: modify layout, all. Add loading

--- app/(main)/all/page.tsx
+++ app/(main)/all/page.tsx
@@ -1,14 +1,14 @@
-
-'use client';
+"use client";
 
 import { useSearchParams } from "next/navigation";
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { Card } from '@/components/ui/card';
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Card } from "@/components/ui/card";
 
-import { AspectRatio } from "@/components/ui/aspect-ratio"
-import Image from "next/image"
-import pic1 from "@/public/app_images/pic1.jpeg"
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import Image from "next/image";
+import pic1 from "@/public/app_images/pic1.jpeg";
+import { Loader } from "lucide-react";
 
 interface Object {
   address: string;
@@ -21,14 +21,14 @@ interface Object {
   district_name: string;
 
   district_id: string;
-  
+
   gmail: string;
   happen_time: string;
   img_url: string;
   object_id: string;
   object_name: string;
   post_time: string;
-  type: 'lost' | 'found'; // type: either lost or found
+  type: "lost" | "found"; // type: either lost or found
   user_id: string;
   user_name: string;
 }
@@ -37,50 +37,64 @@ export default function HomeFilter() {
   const router = useRouter();
   const params = useSearchParams();
   const [objects, setObjects] = useState<Object[]>([]);
+  const [loading, setLoading] = useState(true);
 
   // get search query
-  const search = params.get('search') || '';
+  const search = params.get("search") || "";
 
   // get date query
   // date format : YYYY-MM-DD-YYYY-MM-DD
   // the former is start date, the latter is end date
-  const date = params.get('date') || ''; 
-  const dateArrayStr = date.split('-');
-  const dateArray = dateArrayStr.map(item => parseInt(item, 10));
+  const date = params.get("date") || "";
+  const dateArrayStr = date.split("-");
+  const dateArray = dateArrayStr.map((item) => parseInt(item, 10));
   // convert dateArray from string to int
-  
+
   // get category query, for later filtering
-  const subCategories = params.get('subCategories') || '';
-  const districts_id = params.get('districts') || '';
-  const subCategoriesArray = subCategories.split(',');
-  const districts_idArray = districts_id.split(',');
+  const subCategories = params.get("subCategories") || "";
+  const districts_id = params.get("districts") || "";
+  const subCategoriesArray = subCategories.split(",");
+  const districts_idArray = districts_id.split(",");
 
   const handlePostClick = (id: string) => {
     router.push(`/content/${id}`);
   };
 
   const getObject = async () => {
+    setLoading(true);
     try {
-      const response = await fetch(`/api/object/get?object_id=all&search=${search}`, 
-      {
-        method: "GET",
-      });
+      const response = await fetch(
+        `/api/object/get?object_id=all&search=${search}`,
+        {
+          method: "GET",
+        }
+      );
       const data = await response.json();
       // console.log(data);
       setObjects(data);
     } catch (error) {
       console.error("Network error:", error);
+    } finally {
+      setLoading(false);
     }
-  }
+  };
 
   // filtering
   const filteredObjects = objects.filter((object) => {
     // filter by subCategories
-    if (subCategoriesArray.length > 0 && subCategoriesArray[0] !== '' && !subCategoriesArray.includes(object.category_name)) {
+    if (
+      subCategoriesArray.length > 0 &&
+      subCategoriesArray[0] !== "" &&
+      !subCategoriesArray.includes(object.category_name)
+    ) {
       return false;
     }
     // filter by districts
-    if (districts_idArray.length > 0 && districts_idArray[0] !== '' && !districts_idArray.includes(object.district_id)) {
+    if (
+      districts_idArray.length > 0 &&
+      districts_idArray[0] !== "" &&
+      !districts_idArray.includes(object.district_id)
+    ) {
       return false;
     }
     // filter by date
@@ -93,6 +107,7 @@ export default function HomeFilter() {
         return false;
       }
     }
+    if (object.img_url) console.log(object);
     return true;
   });
 
@@ -103,38 +118,69 @@ export default function HomeFilter() {
   console.log(objects);
 
   return (
-    <div>
-      <div className="w-full bg-white overflow-y-scroll p-4 ">
-        {filteredObjects.map((post) => (
-          <Card key={post.object_id} onClick={() => handlePostClick(post.object_id)} className="mb-4 p-4 bg-gray-100 rounded cursor-pointer">
-            <div className="flex">
-              <div className="w-2/3">
-                <h3 className="font-semibold">{post.object_name}</h3>
-                <span className={`inline-block px-2 py-1 text-xs rounded ${post.type === 'lost' ? 'bg-red-200 text-red-800' : 'bg-green-200 text-green-800'}`}>{post.type}</span>
-                <p className="text-sm text-gray-700 mt-2">{post.description}</p>
-                <div className="mt-2">
-                  <span className="inline-block bg-green-200 text-green-800 text-xs px-2 py-1 rounded-full mr-2">{post.district_name}</span>
-                  <span className="inline-block bg-blue-200  text-blue-800  text-xs px-2 py-1 rounded-full mr-2">{post.category_name}</span>
+    <>
+      {loading ? (
+        <div className="w-full h-full flex justify-center items-center">
+          <Loader className="animate-spin size-10" />
+        </div>
+      ) : (
+        <div className="bg-white  p-4 m-2 rounded-md">
+          {filteredObjects.map((post) => (
+            <Card
+              key={post.object_id}
+              onClick={() => handlePostClick(post.object_id)}
+              className="mb-4 p-4 bg-gray-100 rounded cursor-pointer"
+            >
+              <div className="flex">
+                <div className="w-2/3">
+                  <h3 className="font-semibold">{post.object_name}</h3>
+                  <span
+                    className={`inline-block px-2 py-1 text-xs rounded ${
+                      post.type === "lost"
+                        ? "bg-red-200 text-red-800"
+                        : "bg-green-200 text-green-800"
+                    }`}
+                  >
+                    {post.type}
+                  </span>
+                  <p className="text-sm text-gray-700 mt-2">
+                    {post.description}
+                  </p>
+                  <div className="mt-2">
+                    <span className="inline-block bg-green-200 text-green-800 text-xs px-2 py-1 rounded-full mr-2">
+                      {post.district_name}
+                    </span>
+                    <span className="inline-block bg-blue-200  text-blue-800  text-xs px-2 py-1 rounded-full mr-2">
+                      {post.category_name}
+                    </span>
+                  </div>
                 </div>
-              </div>
                 <div className="w-2/3 h-full relative">
                   <AspectRatio ratio={2 / 1}>
-                    <Image
-                        // src={post.img_url}
-                        src={pic1}
-                        alt="Hero Image"
+                    {post.img_url ? (
+                      <Image
+                        src={post.img_url}
+                        alt={`${post.object_name}的圖片`}
                         fill={true}
                         className="rounded-md object-contain"
-                    />
+                      />
+                    ) : (
+                      <div className="flex aspect-square w-full h-full items-center justify-center rounded-md ">
+                        <div className="flex w-full h-full items-center justify-center bg-gray-200 rounded-md">
+                          <span className="text-gray-500">no image</span>
+                        </div>
+                      </div> // Replace this with your placeholder component
+                    )}
                   </AspectRatio>
                 </div>
-              {/* <div className="w-1/3 ml-4">
+                {/* <div className="w-1/3 ml-4">
                 <img src={post.img_url} alt={post.object_name} className="w-full h-auto rounded" />
               </div> */}
-            </div>
-          </Card>
-        ))}
-      </div>
-    </div>
+              </div>
+            </Card>
+          ))}
+        </div>
+      )}
+    </>
   );
 }
